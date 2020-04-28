1b789d559e7de18e4c4f8a18f25fcdb89fc51401
yong <yong@asial.co.jp>
2020-04-28 19:03:48 +0900

    init

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema.js');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+console.log('Connecting to mongodb');
+mongoose.connect('mongodb+srv://admin:admin@cluster0-1sk1o.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+
+  const PORT = 4000;
+  app.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}`);
+  });
+
+});
