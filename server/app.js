156df588c4d304450fc8c56ea1d589db66310c11
yong <yong@asial.co.jp>
2020-04-29 22:56:58 +0900

    cors

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema.js');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 const app = express();
 
-
+// allows cors
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
