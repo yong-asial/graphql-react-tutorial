c24e632306de9903e0418568d4ef388c63f9aa2c
yong <yong@asial.co.jp>
2020-04-29 22:45:29 +0900

    setup Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
 // components
 import BookList from './components/BookList';
 
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Ninja Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Ninja Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
